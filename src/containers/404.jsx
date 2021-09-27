7c8c0e34905e2dc2b56a8a1578c758b8680fe147
SMMalek <sherif.m.malek@gmail.com>
2021-09-27 18:43:40 +0100

    c18

--- src/containers/404.jsx
+++ src/containers/404.jsx
@@ -1,9 +1,13 @@
 import React from'react'
+import {Navbar} from "../components/navbar";
+import {Footer} from "../components/footer";
 export function NotFoundPage () {
 
     return(
         <div>
+            <Navbar/>
             <h2>404 Not Found!</h2>
+            <Footer/>
         </div>
     );
 }
